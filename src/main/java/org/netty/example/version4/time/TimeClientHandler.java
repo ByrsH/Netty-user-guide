308b0c6f3bdd5deb8b0e007187413de52458bfe4
ByrsH <bgxjsh@sina.com>
2018-06-18 18:31:21 +0800

    add Time client

---
+++ src/main/java/org/netty/example/version4/time/TimeClientHandler.java
@@ -0,0 +1,32 @@
+package org.netty.example.version4.time;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.Date;
+
+/**
+ * @author ByrsH
+ * @create 2018-06-18 17:54
+ **/
+
+public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf m = (ByteBuf) msg;
+        try {
+            Long currentTimeMillis = (m.readUnsignedInt() - 2208988800L) * 1000L;
+            System.out.println(new Date(currentTimeMillis));
+            ctx.close();
+        } finally {
+            m.release();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
